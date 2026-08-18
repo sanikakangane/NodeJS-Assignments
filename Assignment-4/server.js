77987f612d76430ca47ffe4742804227b6cbf530
Sanika Kangane <sanikakangane166@gmail.com>
2026-08-18 23:28:02 +0530

    Replace single quotes with double quotes in server.js

--- Assignment-4/server.js
+++ Assignment-4/server.js
@@ -3,28 +3,27 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 
     if (req.url == '/') {
-        res.end('Welcome to Home Page');
+        res.end("Welcome to Home Page");
     }
 
     else if (req.url == '/about') {
-        res.end('About Us');
+        res.end("About Us");
     }
 
     else if (req.url == '/contact') {
-        res.end('Contact Information');
+        res.end("Contact Information");
     }
 
     else if (req.url == '/services') {
-        res.end('Our Services');
+        res.end("Our Services");
     }
 
     else {
-        res.writeHead(404);
-        res.end('404 - Page Not Found');
+        res.end("404 - Page Not Found");
     }
 
 });
 
 server.listen(4000, () => {
-    console.log('Server is running on http://localhost:4000');
+    console.log("Server is running on http://localhost:4000");
 });
