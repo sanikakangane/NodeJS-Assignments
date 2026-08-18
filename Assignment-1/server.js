5613c5e88677a7672de580d3160e7910880cb9e0
Sanika Kangane <sanikakangane166@gmail.com>
2026-08-18 23:22:43 +0530

    Fix response handling in Node.js server

--- Assignment-1/server.js
+++ Assignment-1/server.js
@@ -1,10 +1,9 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write("Welcome to Node.js Server");
-    res.end();
-})
+    res.end("Welcome to Node.js Server");
+});
 
 server.listen(4000, () => {
     console.log("Server is running on http://localhost:4000");
-})
+});
