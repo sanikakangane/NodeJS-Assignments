88fdc93be74ddbde1fa62f86ba2a492071e4662b
Sanika Kangane <sanikakangane166@gmail.com>
2026-08-18 23:26:35 +0530

    Change server port to 4000 and fix response format

--- Assignment-3/server.js
+++ Assignment-3/server.js
@@ -4,16 +4,18 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
 
     if (req.url == '/student') {
+
         const data = fs.readFileSync('data.json', 'utf-8');
+
         res.end(data);
+
     }
     else {
-        res.writeHead(404);
-        res.end('404 - Page Not Found');
+        res.end("404 - Page Not Found");
     }
 
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+server.listen(4000, () => {
+    console.log("Server is running on http://localhost:4000");
 });
